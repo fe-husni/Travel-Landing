aff082c99841b4dd132a751294f0316396944a67
Husni Mubarok <husniiimubarok00@gmail.com>
2025-07-13 12:41:00 +0700

    fix: update assets path argrgrgrgrg

--- copy-assets.js
+++ copy-assets.js
@@ -2,16 +2,21 @@ const fs = require("fs");
 const path = require("path");
 const fse = require("fs-extra");
 
-// Paths
+// Definisikan path
 const indexSrc = path.join(__dirname, "src", "index.html");
 const indexDist = path.join(__dirname, "dist", "index.html");
 const publicDir = path.join(__dirname, "public");
 const distDir = path.join(__dirname, "dist");
 
-// Copy index.html
+// Pastikan folder dist sudah ada
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir);
+}
+
+// Salin index.html
 fs.copyFileSync(indexSrc, indexDist);
 console.log("✓ Copied index.html");
 
-// Copy public/* to dist/
+// Salin semua isi public/ ke dist/
 fse.copySync(publicDir, distDir, { overwrite: true });
 console.log("✓ Copied /public contents to /dist");
